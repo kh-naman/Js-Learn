b081dcb91b0ba426322a43952ef9a1fff0c6fbdf
nkhandelwal <nkhandelwal@zynga.com>
2025-03-07 20:22:10 +0530

    Adding more func in kanban board

--- kanban-board/kanban.js
+++ kanban-board/kanban.js
@@ -1,13 +1,21 @@
 const dropdown = document.getElementById("category")
 const boardNames = document.querySelectorAll(".board-names")
 const createTaskButton = document.getElementById("create-task-button")
-const tasksContainer = document.querySelectorAll(".tasks-container")
+const taskButtonContainer = document.getElementById("task-button-container")
+const mainContainer = document.querySelector(".main-containers")
 boardNames.forEach((boardName) =>{
-    const category = document.createElement('option');
-    category.value = category.innerText = boardName.innerText;
-    dropdown.appendChild(category)
+    attachNewBoardToDropDown(boardName)
 })
 
+function attachDragEvents(target)
+    {
+        target.addEventListener('dragstart', ()=>{
+            target.classList.add('flying')
+        })
+        target.addEventListener('dragend',()=>{
+            target.classList.remove('flying')
+        })
+    }
 
 createTaskButton.addEventListener("click",()=>{
     const category = document.getElementById("category").value
@@ -23,6 +31,7 @@ createTaskButton.addEventListener("click",()=>{
     const taskHeader = document.createElement('h3')
     taskHeader.innerText = description;
     taskDiv.appendChild(taskHeader)
+    const tasksContainer = document.querySelectorAll(".tasks-container")
     tasksContainer.forEach((taskContainer)=>{
         if(category === taskContainer.id)
         {
@@ -31,22 +40,48 @@ createTaskButton.addEventListener("click",()=>{
         attachDragEvents(taskDiv);
     })
 
-    function attachDragEvents(target)
-    {
-        target.addEventListener('dragstart', ()=>{
-            target.classList.add('flying')
-        })
-        target.addEventListener('dragend',()=>{
-            target.classList.remove('flying')
-        })
-    }
-    
-    const allTaskContainer = document.querySelectorAll(".tasks-container")
-    allTaskContainer.forEach((taskContainer)=>{
-        taskContainer.addEventListener('dragover',()=>{
-            const flyingElement = document.querySelector(".flying")
-            taskContainer.appendChild(flyingElement)
-        })
-    })
 })
+
+const createBoardButton = document.getElementById("create-board-button")
+createBoardButton.addEventListener("click",()=>{
+    const headerDescription = document.getElementById("board-description").value
+    if(headerDescription === "")
+    {
+        alert("Enter board heading")
+        return;
+    }
+    const boardDiv = document.createElement('div')
+    boardDiv.className = "board-container"
+    const header = document.createElement('h2')
+    header.innerText = headerDescription
+    const tasksContainer = document.createElement('div')
+    tasksContainer.className = "tasks-container"
+    tasksContainer.id = headerDescription
+    attachDragEventsToTaskContainer(tasksContainer)
+    boardDiv.appendChild(header)
+    boardDiv.appendChild(tasksContainer);
+    attachNewBoardToDropDown(boardDiv)
+    mainContainer.insertBefore(boardDiv,taskButtonContainer)
+})
+
+const allTaskContainer = document.querySelectorAll(".tasks-container")
+    allTaskContainer.forEach((taskContainer)=>{
+        attachDragEventsToTaskContainer(taskContainer)
+    })
+
+function attachNewBoardToDropDown(targetBoard)
+{   
+    const targetBoardDescription  = targetBoard.querySelector("h2").innerText;
+    const category = document.createElement('option');
+    category.value = category.innerText = targetBoardDescription;
+    dropdown.appendChild(category)
+}
+
+function attachDragEventsToTaskContainer(target)
+{
+    target.addEventListener('dragover',()=>{
+        const flyingElement = document.querySelector(".flying")
+        target.appendChild(flyingElement)
+    })
+}
 
